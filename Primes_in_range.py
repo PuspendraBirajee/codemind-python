0f1338fb562de25ae86aabdd35103c73f4d46aae
Puspendra Birajee <119839786+PuspendraBirajee@users.noreply.github.com>
2023-06-02 22:00:39 +0530

    import math def prime(n):     if(n<2):         return 0     for i in range(2, int(math.sqrt(n)+1)):         if(n%i==0):             return 0     return 1
    n1=int(input())
    n2=int(input())
    c=0
    for i in range(n1, n2+1):
        if(prime(i)):
            c+=1
    print(c)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -2,14 +2,16 @@ import math
 def prime(n):
     if(n<2):
         return 0
-    for i in range(2,int(math.sqrt(n))+1):
+    for i in range(2, int(math.sqrt(n)+1)):
         if(n%i==0):
             return 0
     return 1
-a=int(input())
-b=int(input())
+    
+n1=int(input())
+n2=int(input())
 c=0
-for i in range(a,b+1):
+for i in range(n1, n2+1):
     if(prime(i)):
         c+=1
 print(c)
+    
